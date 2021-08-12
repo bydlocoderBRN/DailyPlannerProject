90d99857d57f015eebca7653eaeb26ce11779fbb
chechkish <dentag240502@mail.ru>
2021-08-12 19:47:24 +0700

    change realization

--- src/PlanerApp/Plan.java
+++ src/PlanerApp/Plan.java
@@ -17,12 +17,14 @@ private ExecutorService pool = Executors.newSingleThreadExecutor();
 Callable<Boolean> callableNotify = new MyCallableNote();
 Future result;
 TreeSet<LocalDateTime> notifications = new TreeSet<LocalDateTime>();
+boolean notificationsIsEmpty;
 Plan(){
     startTime = LocalDateTime.of(1970,1,1,0,0,0);
     finishTime = LocalDateTime.of(1970,1,1,0,0,1);
     head = "";
     content ="";
-    notifications.add(LocalDateTime.of(2100,1,1,0,0,0));
+    notificationsIsEmpty = true;
+    result = pool.submit(callableNotify);
 };
 
 
@@ -38,39 +40,56 @@ public LocalDateTime getFinishTime(){return finishTime;}
 * асинхронный поток (см.ниже) */
 // 1 - note
 // 2 - alarm
+
+    /*Новая реализация:
+    * в конструкторе запускается асинхронный поток с постоянной проверкой пустой ли TreeSet. Как только он ен пустой запускается isNotificationNow.
+    * Когда рекурсия завершается, должно сработать событие, которое запускает оповещение, удаляет notifications.first(), и запускает поток */
 public void addNotification(LocalDateTime note){
     note = note.withNano(1);
     notifications.add(note);
-    result = pool.submit(callableNotify);
+    notificationsIsEmpty = false;
+
 }
-public void addAlarm(LocalDateTime alarm){
+public void addAlarm(LocalDateTime alarm) {
     alarm = alarm.withNano(2);
     notifications.add(alarm);
-    result = pool.submit(callableNotify);}
+    notificationsIsEmpty = false;
+}
 
-public TreeSet<LocalDateTime> getAllNotifications(){return notifications;}
+    public TreeSet<LocalDateTime> getAllNotifications () {
+        return notifications;
+    }
       /*  public LocalDateTime getFirst(){return notifications.first();}
             public LocalDateTime getLast(){return notifications.last();}*/
 
     /* Рекурсивный метод isNotificationNow не прекращается пока время близжайшего уведомления/будильника(first()) не совпадет с временем системы.
-    * Этот метод помещен в отдельный поток для асинхронного выполнения в фоне. Как только метод выходит из петли, первая запись в TreeSet
-    * стирается и result.get() возвращает true */
-public void isNotificationNow(){
-    if (notifications.first().equals(LocalDateTime.now())){
-        notifications.remove(notifications.first());
+     * Этот метод помещен в отдельный поток для асинхронного выполнения в фоне. Как только метод выходит из петли, первая запись в TreeSet
+     * стирается и result.get() возвращает true
+     * т.к. метод каждый раз обращается к notification.first, то при добавлении оповещения, которое должно сработать раньше, метод будет
+     * обрабатывать именно его(т.к. notification.first() дает самое близкое к текущему времени оповещение) */
+    public void isNotificationNow () {
+        if (notifications.first().equals(LocalDateTime.now())) {
+            notifications.remove(notifications.first());
 
-    }else {
-        isNotificationNow(); }
-}
-class MyCallableNote implements Callable<Boolean>{
-    @Override
-    public Boolean call() throws Exception {
-        isNotificationNow();
-        return true;
-            }
+        } else {
+            isNotificationNow();
+        }
+    }
+    public void isNotificationEmpty ()
+    {
+        if (notifications.isEmpty() == false)
+            isNotificationNow();
+        else isNotificationEmpty();
+    }
+    class MyCallableNote implements Callable<Boolean> {
+        @Override
+        public Boolean call() throws Exception {
+            isNotificationEmpty();
+            return true;
+        }
 
     }
-
-
-
 }
+
+
+
